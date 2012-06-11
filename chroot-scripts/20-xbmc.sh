ba6ce8bc344cfe356c0adb207db3595d0b601703
spocky184 <admin@ztreambox.org>
2012-06-11 22:39:23 +0300

    Changed repository to ppa:pulse-eight/pulse

--- chroot-scripts/20-xbmc.sh
+++ chroot-scripts/20-xbmc.sh
@@ -8,16 +8,26 @@ sleep 3
 
 (
 
+if [ ! -f "/etc/apt/sources.list.d/pulse-eight-pulse-precise.list" ]; then
+	apt-add-repository -y ppa:pulse-eight/pulse
+fi
 ISCONFED=`dpkg -s xbmc | grep 'install ok installed'`
 if [ -z "$ISCONFED" ]; then
-	wget http://packages.pulse-eight.net/ubuntu/install-xbmc.sh
-	chmod +x install-xbmc.sh
-	sed -i 's/sudo apt-get -y install xbmc/sudo apt-get -y --force-yes install xbmc/' install-xbmc.sh
-	./install-xbmc.sh
-	apt-get install -y --force-yes afpfs-ng-utils lcdproc libaacs0 libbluray1 libcrystalhd3
-	rm -f install-xbmc.sh
+	apt-get update
+	apt-get install -y --force-yes xbmc afpfs-ng-utils lcdproc libaacs0 libbluray1 libcec libcrystalhd3 libnfs1 librtmp0 libshairport1
 	dpkg-query -W --showformat='${Package} ${Version}\n' > 12-xbmc-packages.txt
 fi
+
+#ISCONFED=`dpkg -s xbmc | grep 'install ok installed'`
+#if [ -z "$ISCONFED" ]; then
+#	wget http://packages.pulse-eight.net/ubuntu/install-xbmc.sh
+#	chmod +x install-xbmc.sh
+#	sed -i 's/sudo apt-get -y install xbmc/sudo apt-get -y --force-yes install xbmc/' install-xbmc.sh
+#	./install-xbmc.sh
+#	apt-get install -y --force-yes afpfs-ng-utils lcdproc libaacs0 libbluray1 libcrystalhd3
+#	rm -f install-xbmc.sh
+#	dpkg-query -W --showformat='${Package} ${Version}\n' > 12-xbmc-packages.txt
+#fi
 
 # if [ ! -f "/etc/apt/sources.list.d/alexandr-surkov-xbmc-pvr-precise.list" ]; then
 	# apt-add-repository -y ppa:alexandr-surkov/xbmc-pvr
